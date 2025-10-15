6e01059873dd62111b7b51c2a1f03c0f4d1f6d52
Hammadi-Aya01 <hammadiaya2004@gmail.com>
2025-10-15 17:51:59 +0100

    comm

--- src/clientp/Client.java
+++ src/clientp/Client.java
@@ -1,5 +1,65 @@
 package clientp;
 
-public class Client {
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Client {
+    public static void main(String[] args) {
+        String SERVER_IP = "192.168.56.1";
+
+        try {
+            InetAddress serverAddress = InetAddress.getByName(SERVER_IP);
+            InetSocketAddress socketAddress = new InetSocketAddress(serverAddress, 12340);
+            Socket socket = new Socket();
+            socket.connect(socketAddress);
+
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            Scanner scanner = new Scanner(System.in);
+
+            while (true) {
+                System.out.println("1.  + ");
+                System.out.println("2.  - ");
+                System.out.println("3.  * ");
+                System.out.println("4.  / ");
+                System.out.println("0. Quitter");
+                System.out.print("Votre choix (0-4) : ");
+
+                int choix = scanner.nextInt();
+
+                if (choix == 0) {
+                    out.writeInt(0);
+                    break;
+                }
+
+                if (choix < 1 || choix > 4) {
+                    System.out.println("Choix invalide !");
+                    continue;
+                }
+
+                System.out.print("Entrez la valeur de x : ");
+                int x = scanner.nextInt();
+
+                out.writeInt(choix);
+                out.writeInt(x);
+
+                int resultat = in.readInt();
+                System.out.println("Resultat : " + resultat);
+            }
+
+            out.close();
+            in.close();
+            socket.close();
+            System.out.println("Deconnexion terminee.");
+
+        } catch (IOException e) {
+            System.err.println("Erreur de connexion au serveur.");
+            e.printStackTrace();
+        }
+    }
 }
--- src/serverp/Server.java
+++ src/serverp/Server.java
@@ -1,5 +1,69 @@
 package serverp;
 
-public class Server {
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 
+public class Server {
+    public static void main(String[] args) {
+        try {
+            InetAddress bindAddress = InetAddress.getByName("0.0.0.0");
+            InetSocketAddress socketAddress = new InetSocketAddress(bindAddress, 12340);
+            ServerSocket socketserveur = new ServerSocket();
+            socketserveur.bind(socketAddress);
+
+            System.out.println("Serveur en attente...");
+            Socket socket = socketserveur.accept();
+            System.out.println("Client connecté : " + socket.getInetAddress());
+
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            while (true) {
+            	int n1 = in.read();
+                String op= in.readUTF(); //na9rou byhe chaine 'readUTF'
+                int n2= in.read();
+                if ("EXIT".equals(op)) {
+                    System.out.println("Client déconnecté.");
+                    break;
+                }
+
+                int res = 0;
+
+                switch (op) {
+                    case "+":
+                        res = n1 + n2;
+                        break;
+                    case "-":
+                        res= n1 - n2;
+                        break;
+                    case "*":
+                        res= n1 * n2;
+                        break;
+                    case "/":
+                        if (5 != 0) {
+                            res = n1 / n2;
+                        }
+                        break;
+                    default:
+                        res = 0;
+                }
+
+                out.writeInt(res);
+                System.out.println(n1 + op + n2+ "="+ res);
+            }
+
+            in.close();
+            out.close();
+            socket.close();
+            socketserveur.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
